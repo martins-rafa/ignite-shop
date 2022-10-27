36c6974adc84f319d6480b1525eb7d37aaee0e78
Rafa Martins <rafamartins.dev@gmail.com>
2022-10-27 09:54:17 -0300

    use Header Component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,15 @@
 import { AppProps } from "next/app";
+import { Header } from "../components/Header";
 import { globalStyles } from "../styles/global";
 
-import logo from "../assets/logo.svg";
-import { Container, Header } from "../styles/pages/app";
-import Image from "next/future/image";
+import { Container } from "../styles/pages/app";
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
-      <Header>
-        <Image src={logo} alt="" />
-      </Header>
+      <Header />
       <Component {...pageProps} />
     </Container>
   );
